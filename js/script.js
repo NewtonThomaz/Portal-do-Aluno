f75e65d35b03cafb6db63854076e398b1087e0bd
Newton André Thomaz <159278087+NewtonThomaz@users.noreply.github.com>
2025-04-28 02:37:57 -0300

    login
    login feito, porém não é totalmente responsivo na versão desktop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
-// Arquivo de script vazio 
+
+const senha = document.getElementById('senha');
+const olho = document.getElementById('olho');
+function verSenha() {
+
+    if (olho.classList.contains('olho')) {
+        senha.setAttribute('type', 'text');
+        olho.setAttribute('src', 'img/eye.jpg');
+        olho.setAttribute('class', 'olhoAberto');
+    } else {
+
+        senha.setAttribute('type', 'password');
+        olho.setAttribute('src', 'img/closedeye.jpg');
+        olho.setAttribute('class', 'olho');
+    }
+}
+
+function entrar() {
+    const usuarioV = document.getElementById('usuario').value.trim();
+    const senhaV = document.getElementById('senha').value.trim();
+    const usuarioE = document.getElementById('usuarioE');
+    const entrar = document.getElementById('entrar');
+    const senhaE = document.getElementById('senhaE');
+    let validaLogin = true;
+
+    // Array de usuários e senhas válidos
+    const usuariosValidos = ["adm", "user1", "user2"];
+    const senhasValidas = ["adm", "user1", "user2"];
+    
+    if (usuarioV === "") {
+        usuarioE.classList.remove('oculto');
+    } else if (senhaV === "") {
+        senhaE.classList.remove('oculto');
+        usuarioE.classList.add('oculto');
+    } else {
+        for (let i = 0; i < usuariosValidos.length; i++) {
+            if (usuarioV === usuariosValidos[i] && senhaV === senhasValidas[i]) {
+                window.location.href = 'pages/inicio.html';
+                validaLogin = false;
+            }
+        } if (validaLogin) {
+            alert('Usuário ou senha inválidos!');
+        }
+    }
+}
