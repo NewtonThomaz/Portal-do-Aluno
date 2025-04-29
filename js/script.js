2f3ddd6ecb40d5e0a5e7cdb756e1cfe65a0f79ed
Newton André Thomaz <159278087+NewtonThomaz@users.noreply.github.com>
2025-04-29 07:14:47 -0300

    login 2

--- js/script.js
+++ js/script.js
@@ -15,18 +15,19 @@ function verSenha() {
     }
 }
 
+let validaLogin = true;
+
+// Array de usuários e senhas válidos
+const usuariosValidos = ["adm", "user1", "user2"];
+const senhasValidas = ["adm", "user1", "user2"];
+
+// entrar.addEventListener("click",
 function entrar() {
     const usuarioV = document.getElementById('usuario').value.trim();
     const senhaV = document.getElementById('senha').value.trim();
     const usuarioE = document.getElementById('usuarioE');
-    const entrar = document.getElementById('entrar');
     const senhaE = document.getElementById('senhaE');
-    let validaLogin = true;
-
-    // Array de usuários e senhas válidos
-    const usuariosValidos = ["adm", "user1", "user2"];
-    const senhasValidas = ["adm", "user1", "user2"];
-    
+    const entrar = document.getElementById('entrar');
     if (usuarioV === "") {
         usuarioE.classList.remove('oculto');
     } else if (senhaV === "") {
@@ -43,3 +44,29 @@ function entrar() {
         }
     }
 }
+// );
+document.addEventListener('keydown', function (e) {
+    const usuarioV = document.getElementById('usuario').value.trim();
+    const senhaV = document.getElementById('senha').value.trim();
+    const usuarioE = document.getElementById('usuarioE');
+    const senhaE = document.getElementById('senhaE');
+    const entrar = document.getElementById('entrar');
+    
+    if (e.key === 'Enter') {
+        if (usuarioV === "") {
+            usuarioE.classList.remove('oculto');
+        } else if (senhaV === "") {
+            senhaE.classList.remove('oculto');
+            usuarioE.classList.add('oculto');
+        } else {
+            for (let i = 0; i < usuariosValidos.length; i++) {
+                if (usuarioV === usuariosValidos[i] && senhaV === senhasValidas[i]) {
+                    window.location.href = 'pages/inicio.html';
+                    validaLogin = false;
+                }
+            } if (validaLogin) {
+                alert('Usuário ou senha inválidos!');
+            }
+        }
+    }
+});
